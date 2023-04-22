f52ae7e2b2392c270dc44380eb28856d6b4ee1ba
Joshua Sibanda <75424803+joshua-inf@users.noreply.github.com>
2023-04-22 11:30:39 +0200

    additions

--- src/payments/useMuiltistepsForm.js
+++ src/payments/useMuiltistepsForm.js
@@ -1,19 +1,35 @@
-import { ReactElement, useState } from "react";
+// import { ReactElement, useState } from "react";
 
 
-export function useMultistepForm(){
-    const [currentStepIndex, setCurentStepIndex] = useState(0)
+// export function useMultistepForm(steps){
+//     const [currentStepIndex, setCurentStepIndex] = useState(0)
 
 
-    function next() {
 
-    }
+//     function next() {
+//         setCurentStepIndex(i = {
+//             if(i >= steps.length - 1){
+                
+//                 return (<>
+                
+//                 </>)
+//             }
+//         })
+//     }
 
-    function back() {
+//     function back() {
 
-    }
+//     }
 
-    function goTo(){
+//     function goTo(index){
+//         setCurentStepIndex(index)
+//     }
 
-    }
-}
+
+//     return [
+//         currentStepIndex,
+//         steps[currentStepIndex],
+//         goTo
+
+//     ]
+// }
